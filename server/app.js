fa3a9d515fe07938b4795f7081f5ffe17e6c2c75
Skander Msalmi <msalmiskander@gmail.com>
2024-01-07 12:46:21 +0100

    init env

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+
+
+app.use('/api', indexRouter);
+
+
+module.exports = app;
